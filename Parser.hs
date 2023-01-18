963d24ed567d7a06dfbf1400185f15b583c57bbc
mierzej <mmierzejewski.marcin@gmail.com>
2023-01-19 00:17:07 +0100

    remove index parameter from Group

--- Parser.hs
+++ Parser.hs
@@ -11,16 +11,16 @@ data Regex
     | Or    Regex Regex
     | Cat   Regex Regex
     | Star  Regex
-    | Group Int (Maybe String) Regex -- to-be Nat instead of Int
+    | Group (Maybe String) Regex
 
 instance Eq Regex where
-    Eps            == Eps            = True
-    Lit   s  _     == Lit   t  _     =  s == t
-    Or    ll rl    == Or    lr rr    = ll == lr && rl == rr
-    Cat   ll rl    == Cat   lr rr    = ll == lr && rl == rr
-    Star  re       == Star  rf       = re == rf
-    Group n  lb re == Group m  lc rf = lb == lc && re == rf
-    _              == _              = False
+    Eps         == Eps         = True
+    Lit   s  _  == Lit   t  _  =  s == t
+    Or    ll rl == Or    lr rr = ll == lr && rl == rr
+    Cat   ll rl == Cat   lr rr = ll == lr && rl == rr
+    Star  re    == Star  rf    = re == rf
+    Group lb re == Group lc rf = lb == lc && re == rf
+    _           == _           = False
 
 instance Show Regex where
     -- show Eps          = "Eps"
@@ -28,40 +28,40 @@ instance Show Regex where
     -- show (Or    l  r) = "Or ("  ++ show l ++ ") (" ++ show r ++ ")"
     -- show (Cat   l  r) = "Cat (" ++ show l ++ ") (" ++ show r ++ ")"
     -- show (Star  re)   = "Star (" ++ show re ++ ")"
-    -- show (Group n  Nothing  re) = "Group " ++ show n ++ " <nameless> (" ++ show re ++ ")"
-    -- show (Group n (Just lb) re) = "Group " ++ show n ++ " \"" ++ lb ++ "\" (" ++ show re ++ ")"
+    -- show (Group Nothing   re) = "Group " ++ show n ++ " <nameless> ("       ++ show re ++ ")"
+    -- show (Group (Just lb) re) = "Group " ++ show n ++ " \"" ++ lb ++ "\" (" ++ show re ++ ")"
     show Eps            = ""
-    show (Lit   s   _)  = s
+    show (Lit   s  _)   = s
     show (Or    re Eps) = show $ Or Eps re
     show (Or    Eps re) = case re of
-        Group _ _ _ -> show re ++ "?"
-        _           -> "(" ++ show re ++ ")?"
+        Group {} ->        show re ++  "?"
+        _        -> "(" ++ show re ++ ")?"
     show (Or    l   r)  = show l ++ "|" ++ show r
     show (Cat   re  (Star rf)) | re == rf = show re ++ "+"
     show (Cat   l   r)  = show l ++ show r
     show (Star  re)     = show re ++ "*"
-    show (Group _ _ re) = "(" ++ show re ++ ")"
+    show (Group _   re) = "(" ++ show re ++ ")"
 
 
 main = print $ parse "a*|(|b(x|d|p)c)?d+|e*"
 
 simpl :: Regex -> Regex
+simpl (Or    re  rf)  = Or   (simpl re) (simpl rf)
 simpl (Cat   Eps re)  = simpl re
 simpl (Cat   re  Eps) = simpl re
-simpl (Cat   re  rf)  = Cat (simpl re) (simpl rf)
-simpl (Or    re  rf)  = Or  (simpl re) (simpl rf)
-simpl (Star  re)      = Star $ simpl re
-simpl (Group n lb re) = Group n lb $ simpl re
-simpl re = re
+simpl (Cat   re  rf)  = Cat  (simpl re) (simpl rf)
+simpl (Star  re)      = Star (simpl re)
+simpl (Group lb  re)  = Group lb (simpl re)
+simpl re              = re
 
 parse :: String -> Maybe Regex
 parse s = case auxparse True Eps s of
     Nothing      -> Nothing
     Just (re, _) -> Just $ simpl re
 
--- fold-analogous
+-- foldl-analogous
 auxparse :: Bool -> Regex -> String -> Maybe (Regex, String)
-auxparse _        acc [] = Just (acc, [])
+auxparse failhard acc [] = Just (acc, [])
 auxparse failhard acc s  = case subparse failhard s of
     Nothing -> if failhard then Nothing else Just (acc, s)
     Just (merger, s') -> auxparse failhard (merger acc) s'
@@ -77,7 +77,7 @@ alt _ _      = Nothing
 
 group :: String -> Maybe (Regex, Regex -> Regex -> Regex, String)
 group ('(':s) = case auxparse False Eps s of
-    Just (re, ')':s') -> Just (Group 0 Nothing re, Cat, s')
+    Just (re, ')':s') -> Just (Group Nothing re, Cat, s')
     _  -> Nothing
 group _ = Nothing
 
