aea2b1e3f420007b1443cba67888ba6e9496b5fb
mierzej <mmierzejewski.marcin@gmail.com>
2023-02-05 12:32:25 +0100

    add id parameter to groups

--- Parser.hs
+++ Parser.hs
@@ -13,40 +13,40 @@ data Regex
     | Or    Regex  Regex
     | Cat   Regex  Regex
     | Star  Regex
-    | Group String Regex
+    | Group Int String Regex
 
 instance Eq Regex where
-    Eps         == Eps         = True
-    Lit   s  _  == Lit   t  _  =  s == t
-    Or    ll rl == Or    lr rr = ll == lr && rl == rr
-    Cat   ll rl == Cat   lr rr = ll == lr && rl == rr
-    Star  re    == Star  rf    = re == rf
-    Group lb re == Group lc rf = lb == lc && re == rf
-    _           == _           = False
+    Eps            == Eps            = True
+    Lit   s  _     == Lit   t  _     =  s == t
+    Or    ll rl    == Or    lr rr    = ll == lr && rl == rr
+    Cat   ll rl    == Cat   lr rr    = ll == lr && rl == rr
+    Star  re       == Star  rf       = re == rf
+    Group id lb re == Group ie lc rf = lb == lc && re == rf
+    _              == _              = False
 
 instance Show Regex where
     -- show = showStruct
     show = stringify
         where
-            showStruct Eps           = "Eps"
-            showStruct (Lit   s  _)  = "Lit \"" ++ s ++ "\" <pred>"
-            showStruct (Or    l  r)  = "Or ("  ++ show l ++ ") (" ++ show r ++ ")"
-            showStruct (Cat   l  r)  = "Cat (" ++ show l ++ ") (" ++ show r ++ ")"
-            showStruct (Star  re)    = "Star (" ++ show re ++ ")"
-            showStruct (Group lb re) = "Group \"" ++ lb ++ "\" (" ++ show re ++ ")"
+            showStruct Eps              = "Eps"
+            showStruct (Lit   s  _)     = "Lit \"" ++ s ++ "\" <pred>"
+            showStruct (Or    l  r)     = "Or ("  ++ show l ++ ") (" ++ show r ++ ")"
+            showStruct (Cat   l  r)     = "Cat (" ++ show l ++ ") (" ++ show r ++ ")"
+            showStruct (Star  re)       = "Star (" ++ show re ++ ")"
+            showStruct (Group id lb re) = "Group " ++ show id ++ " \"" ++ lb ++ "\" (" ++ show re ++ ")"
 
-            stringify Eps           = ""
-            stringify (Lit  s   _)  = s
-            stringify (Or   re Eps) = show $ Or Eps re
-            stringify (Or   Eps re) = case re of
+            stringify Eps             = ""
+            stringify (Lit  s   _)    = s
+            stringify (Or   re Eps)   = show $ Or Eps re
+            stringify (Or   Eps re)   = case re of
                 Group {} ->        show re ++  "?"
                 _        -> "(" ++ show re ++ ")?"
-            stringify (Or   l   r)  = show l ++ "|" ++ show r
+            stringify (Or   l   r)    = show l ++ "|" ++ show r
             stringify (Cat  re  (Star rf)) | re == rf = show re ++ "+"
-            stringify (Cat  l   r)  = show l ++ show r
-            stringify (Star re)     = show re ++ "*"
-            stringify (Group [] re) = "("               ++ show re ++ ")"
-            stringify (Group lb re) = "(<" ++ lb ++ ">" ++ show re ++ ")"
+            stringify (Cat  l   r)    = show l ++ show r
+            stringify (Star re)       = show re ++ "*"
+            stringify (Group _ "" re) = "("               ++ show re ++ ")"
+            stringify (Group _ lb re) = "(<" ++ lb ++ ">" ++ show re ++ ")"
 
 
 -- main = print $ parse "abcdefghi"
@@ -59,13 +59,13 @@ main = print $ parse "a*|(<NAMEK>|b(x|d|p\\w)c\\d)?d+()|e*"
 -- main = print $ parse "a*(b(x)c)?d+()"
 
 simpl :: Regex -> Regex
-simpl (Or    re  rf)  = Or   (simpl re) (simpl rf)
-simpl (Cat   Eps re)  = simpl re
-simpl (Cat   re  Eps) = simpl re
-simpl (Cat   re  rf)  = Cat  (simpl re) (simpl rf)
-simpl (Star  re)      = Star (simpl re)
-simpl (Group lb  re)  = Group lb (simpl re)
-simpl re              = re
+simpl (Or    re  rf)   = Or   (simpl re) (simpl rf)
+simpl (Cat   Eps re)   = simpl re
+simpl (Cat   re  Eps)  = simpl re
+simpl (Cat   re  rf)   = Cat  (simpl re) (simpl rf)
+simpl (Star  re)       = Star (simpl re)
+simpl (Group id lb re) = Group id lb (simpl re)
+simpl re               = re
 
 parse :: String -> Maybe Regex
 parse s = case auxparse True s of
@@ -128,7 +128,7 @@ plainOrEscd _  = Nothing
 
 group :: String -> Maybe (Regex, Regex -> Regex -> Regex, String)
 group ('(':s) = case auxparse False s' of
-    Just (re, glue, ')':s'') -> Just (Eps `glue` Group name re, Cat, s'')
+    Just (re, glue, ')':s'') -> Just (Eps `glue` Group 0 name re, Cat, s'')
     _ -> Nothing
     where
         (name, s') = case parseName s of
